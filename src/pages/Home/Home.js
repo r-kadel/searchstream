2acc4b16b15c1f284f10bc8afb675d359ceb4472
Ryan Kadel <ryan.kadel88@gmail.com>
2020-02-05 06:01:41 -0500

    added error boundary

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,15 +1,15 @@
-import React, {useState, useContext} from 'react'
+import React, { useState, useContext } from 'react'
 import './Home.css'
-import {Context} from '../../Context'
+import { Context } from '../../Context'
 import Result from '../../components/Result/Result'
-
+import SearchError from '../../components/ErrorBoundary/SearchError'
 
 function Home() {
   const [searchTerm, setSearchTerm] = useState('')
-  const {searchResults, getSearchResults} = useContext(Context)
+  const { searchResults, getSearchResults } = useContext(Context)
 
   function handleChange(e) {
-    const {value} = e.target
+    const { value } = e.target
     setSearchTerm(value)
   }
 
@@ -19,34 +19,32 @@ function Home() {
   }
 
   const results = searchResults.map(result => (
-        <Result key={result.id} data={result} /> 
-        ))
+    <Result key={result.id} data={result} />
+  ))
 
   return (
-    <main className="content">
-      <section className="search-wrapper">
-        <h1>SearchStream</h1>
-        <form
-          onSubmit={handleSubmit}
-          className='search-form'
-        >
-          <label htmlFor="search-bar">Enter your search here:</label>
-            <input 
+    <SearchError>
+      <main className="content">
+        <section className="search-wrapper">
+          <h1>SearchStream</h1>
+          <form onSubmit={handleSubmit} className="search-form">
+            <label htmlFor="search-bar">Enter your search here:</label>
+            <input
               name="search-bar"
               className="search-bar"
               type="text"
               onChange={handleChange}
-              required 
+              required
             />
-          <button type='submit' id="search-page-btn">Search</button>
-        </form>
-      </section>
+            <button type="submit" id="search-page-btn">
+              Search
+            </button>
+          </form>
+        </section>
 
-      <section className="results-section">
-        {results}
-      </section>
-
-    </main>
+        <section className="results-section">{results}</section>
+      </main>
+    </SearchError>
   )
 }
 
